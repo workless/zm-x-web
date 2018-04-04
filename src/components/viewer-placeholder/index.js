3410aa63f26df7e6ff301d1602ce53adaf878a0d
Peter Lenahan <peter.lenahan@synacor.com>
2018-04-04 12:29:35 -0400

    PREAPPS-511: Replace the viewer placeholder icon with a real icon of a different envelope.

--- src/components/viewer-placeholder/index.js
+++ src/components/viewer-placeholder/index.js
@@ -1,10 +1,12 @@
 import { h } from 'preact';
+import { Icon } from '@zimbra/blocks';
 import PropTypes from 'prop-types';
 
 import s from './style.less';
 
 const ViewerPlaceholder = ({ numSelected }) => (
 	<div class={s.placeholder}>
+		<Icon name="envelope" />
 		{numSelected > 0 &&
 			<div class={s.numOverlay}>
 				{numSelected}
