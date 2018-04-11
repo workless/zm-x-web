fb9e69eb1193d1991f7bd73165ab2e5f548a8a84
Yang Cai <yang.cai@synacor.com>
2018-04-11 11:58:13 -0400

    - removed media center related tests into media-center.js - updated new inject soap api call

--- tests/e2e/testcafe/mail.js
+++ tests/e2e/testcafe/mail.js
@@ -9,6 +9,7 @@ import { dialog } from './page-model/dialog';
 import { elements } from './page-model/elements';
 import { soap } from './utils/soap-client';
 import LmtpClient from './utils/lmtp-client';
+import Inject from './utils/Inject-msg';
 const path = require('path');
 
 /***************************/
@@ -195,113 +196,6 @@ test('L1 | Move user created folder from context menu into/out from another fold
 });
 
 
-/************************************/
-/*** Mail: Compose scroll fixture ***/
-/************************************/
-
-fixture `Mail: Compose scroll fixture`
-	.page(profile.hostURL)
-	.before( async ctx => {
-		ctx.adminAuthToken = await soap.getAdminAuthToken();
-	})
-	.beforeEach( async t => {
-		t.ctx.user = await soap.createAccount(t.fixtureCtx.adminAuthToken);
-		t.ctx.userAuth = await soap.getUserAuthToken(t.ctx.user.email, t.ctx.user.password);
-		const lmtp = new LmtpClient();
-		const filePath = path.join(__dirname, './data/mime/emails/empty.txt');
-		await lmtp.send(t.ctx.user.email, filePath);
-		await t.resizeWindow(1200,600);
-		await actions.loginEmailPage(t.ctx.user.email, t.ctx.user.password);
-		await t.expect(sidebar.checkSidebarItemExists('Inbox')).ok({ timeout: 15000 });
-	})
-	.afterEach( async t  => {
-		await soap.deleteAccount(t.ctx.user.id, t.fixtureCtx.adminAuthToken);
-	});
-
-test('L2 | Images tab, Endless Scroll | C565544', async t => {
-	await mail.openEmail(0);
-	await mail.clickToolbarButton(0);
-	await compose.clickPlusSign();
-	const startRectTop = await elements.plusSignMenuPhotoFromEmailAreaItemButton.nth(0).getBoundingClientRectProperty('top');
-	await utilFunc.scrollElement.with({ dependencies: { scrollPosition: 'down' } })(elements.plusSignScrollVirtualListSelector);
-	await t.wait(1000);
-	const endRectTop = await elements.plusSignMenuPhotoFromEmailAreaItemButton.nth(0).getBoundingClientRectProperty('top');
-	await utilFunc.scrollElement.with({ dependencies: { scrollPosition: 'up' } })(elements.plusSignScrollVirtualListSelector);
-	await t
-		.expect(await elements.plusSignMenuPhotoFromEmailAreaItemButton.nth(0).getBoundingClientRectProperty('top')).eql(startRectTop)
-		.expect(startRectTop > endRectTop).ok();
-})
-	.before( async t => {
-		t.ctx.user = await soap.createAccount(t.fixtureCtx.adminAuthToken);
-		t.ctx.userAuth = await soap.getUserAuthToken(t.ctx.user.email, t.ctx.user.password);
-		const lmtp = new LmtpClient();
-		const filePath = path.join(__dirname, './data/mime/emails/images.txt');
-		await lmtp.send(t.ctx.user.email, filePath);
-		await t.resizeWindow(1200,600);
-		await actions.loginEmailPage(t.ctx.user.email, t.ctx.user.password);
-		await t.expect(sidebar.checkSidebarItemExists('Inbox')).ok({ timeout: 15000 });
-	});
-
-test('L2 | Files tab, Endless Scroll | C565545', async t => {
-	let fileName = 'PDFFile.pdf';
-	await mail.openEmail(0);
-	await mail.clickToolbarButton(0);
-	await compose.clickPlusSign();
-	await compose.clickPlusSignMenuNavItem(1);
-	const startRectTop = await elements.plusSignMenuFileFromEmailAreaItemButton.withAttribute('title', fileName).getBoundingClientRectProperty('top');
-	await utilFunc.scrollElement.with({ dependencies: { scrollPosition: 'down' } })(elements.plusSignScrollVirtualListSelector);
-	await t.expect(elements.plusSignMenuFileFromEmailAreaItemButton.withAttribute('title', 'ExcelDocFile.xlsx').exists).ok(); // Check last file in list exists
-	const endRectTop = await elements.plusSignMenuFileFromEmailAreaItemButton.withAttribute('title', fileName).getBoundingClientRectProperty('top');
-	await utilFunc.scrollElement.with({ dependencies: { scrollPosition: 'up' } })(elements.plusSignScrollVirtualListSelector);
-	await t
-		.expect(await elements.plusSignMenuFileFromEmailAreaItemButton.withAttribute('title', fileName).getBoundingClientRectProperty('top')).eql(startRectTop)
-		.expect(startRectTop > endRectTop).ok();
-})
-	.before( async t => {
-		t.ctx.user = await soap.createAccount(t.fixtureCtx.adminAuthToken);
-		t.ctx.userAuth = await soap.getUserAuthToken(t.ctx.user.email, t.ctx.user.password);
-		const lmtp = new LmtpClient();
-		const filePath = path.join(__dirname, './data/mime/emails/attachments.txt');
-		await lmtp.send(t.ctx.user.email, filePath);
-		await t.resizeWindow(1200,600);
-		await actions.loginEmailPage(t.ctx.user.email, t.ctx.user.password);
-		await t.expect(sidebar.checkSidebarItemExists('Inbox')).ok({ timeout: 15000 });
-	});
-
-test('L2 | GIF tab, Endless Scroll | C565546', async t => {
-	let buttonText = 'thumbs up';
-	await mail.openEmail(0);
-	await mail.clickToolbarButton(0);
-	await compose.clickPlusSign();
-	await compose.clickPlusSignMenuNavItem(2);
-	await compose.clickSuggestedSearchButton(buttonText);
-	const scrollItemCount = await elements.plusSignMenuGifsItemButton.count;
-	const startRectTop = await elements.plusSignMenuGifsItemButton.nth(0).getBoundingClientRectProperty('top');
-	await utilFunc.scrollElement.with({ dependencies: { scrollPosition: 'down' } })(elements.plusSignScrollVirtualListSelector.nth(0));
-	await t
-		.wait(500)
-		.expect(elements.plusSignMenuGifsItemButton.nth(scrollItemCount).exists).ok({ timeout: 10000 });
-	const endRectTop = await elements.plusSignMenuGifsItemButton.nth(0).getBoundingClientRectProperty('top');
-	await t.expect(startRectTop > endRectTop);
-});
-
-test.skip('L2 | Web link tab, Endless Scroll | C565547 | PREAPPS-305', async t => {
-	let searchText = 'shopping';
-	await mail.openEmail(0);
-	await mail.clickToolbarButton(0);
-	await compose.clickPlusSign();
-	await compose.clickPlusSignMenuNavItem(3);
-	await compose.clickSuggestedSearchButton(searchText);
-	const scrollItemCount = await elements.plusSignMenuSearchesItemButton.count;
-	const startRectTop = await elements.plusSignMenuSearchesItemButton(0).getBoundingClientRectProperty('top');
-	await utilFunc.scrollElement.with({ dependencies: { scrollPosition: 'down' } })(elements.plusSignScrollVirtualListSelector.nth(0));
-	await t
-		.wait(500)
-		.expect(elements.plusSignMenuSearchesItemButton(scrollItemCount).exists).ok({ timeout: 10000 });
-	const endRectTop = await elements.plusSignMenuSearchesItemButton(0).getBoundingClientRectProperty('top');
-	await t.expect(startRectTop > endRectTop);
-});
-
 /******************************/
 /*** Mail: Rich Text Editor ***/
 /******************************/
@@ -314,9 +208,9 @@ fixture `Mail: Reply, Rich Text Editor fixture`
 	.beforeEach( async t => {
 		t.ctx.user = await soap.createAccount(t.fixtureCtx.adminAuthToken);
 		t.ctx.userAuth = await soap.getUserAuthToken(t.ctx.user.email, t.ctx.user.password);
-		const lmtp = new LmtpClient();
+		const inject = new Inject();
 		const filePath = path.join(__dirname, './data/mime/emails/empty.txt');
-		await lmtp.send(t.ctx.user.email, filePath);
+		inject.send(t.ctx.userAuth, filePath);
 		await t.maximizeWindow();
 		await actions.loginEmailPage(t.ctx.user.email, t.ctx.user.password);
 		await t.expect(sidebar.checkSidebarItemExists('Inbox')).ok({ timeout: 15000 });
@@ -355,53 +249,6 @@ test.skip('L1 | Responsive Composer Toolbar | C612378 | PREAPPS-206', async t =>
 		.wait(2000);
 	await t
 		.expect(await elements.componentsToolbarMiddleSelector.child().count).eql(toolbarItemCount);
-});
-
-test('L2 | Attachments > Attach Photo From Email | C769871', async t => {
-	await t.expect(elements.componentsToolbarMiddleSelector.exists).ok({ timeout: 10000 });
-	await compose.selectComposeToolbarPopmenu('Attachments', 'Attach Photo From Email');
-	await t.expect(elements.plusSignMenuPhotoFromEmailAreaItemButton.nth(0).exists).ok({ timeout: 10000 });
-})
-	.before( async t => {
-		t.ctx.user = await soap.createAccount(t.fixtureCtx.adminAuthToken);
-		t.ctx.userAuth = await soap.getUserAuthToken(t.ctx.user.email, t.ctx.user.password);
-		const lmtp = new LmtpClient();
-		const filePath = path.join(__dirname, './data/mime/emails/images.txt');
-		await lmtp.send(t.ctx.user.email, filePath);
-		await actions.loginEmailPage(t.ctx.user.email, t.ctx.user.password);
-		await t.expect(sidebar.checkSidebarItemExists('Inbox')).ok({ timeout: 15000 });
-		await mail.openEmail(0);
-		await mail.clickToolbarButton(0);
-	});
-
-test('L2 | Attachments > Attach File From Email | C769872', async t => {
-	let fileName = 'WordDocFile.docx';
-	await t.expect(elements.componentsToolbarMiddleSelector.exists).ok({ timeout: 10000 });
-	await compose.selectComposeToolbarPopmenu('Attachments', 'Attach File From Email');
-	await t.expect(elements.plusSignMenuFileFromEmailAreaItemButton.withAttribute('title', fileName).exists).ok({ timeout: 10000 });
-})
-	.before( async t => {
-		t.ctx.user = await soap.createAccount(t.fixtureCtx.adminAuthToken);
-		t.ctx.userAuth = await soap.getUserAuthToken(t.ctx.user.email, t.ctx.user.password);
-		const lmtp = new LmtpClient();
-		const filePath = path.join(__dirname, './data/mime/emails/attachments.txt');
-		await lmtp.send(t.ctx.user.email, filePath);
-		await actions.loginEmailPage(t.ctx.user.email, t.ctx.user.password);
-		await t.expect(sidebar.checkSidebarItemExists('Inbox')).ok({ timeout: 15000 });
-		await mail.openEmail(0);
-		await mail.clickToolbarButton(0);
-	});
-
-test('L2 | Attachments > Attach GIF | C769873', async t => {
-	await t.expect(elements.componentsToolbarMiddleSelector.exists).ok({ timeout: 10000 });
-	await compose.selectComposeToolbarPopmenu('Attachments', 'Attach GIF');
-	await t.expect(elements.plusSignMenuPopularGIFsItemButton.nth(0).exists).ok({ timeout: 10000 });
-});
-
-test('L2 | Attachments > Attach Web Link | C769874', async t => {
-	await t.expect(elements.componentsToolbarMiddleSelector.exists).ok({ timeout: 10000 });
-	await compose.selectComposeToolbarPopmenu('Attachments', 'Attach Web Link');
-	await t.expect(elements.buttonWithText('shopping').exists).ok({ timeout: 10000 });
 });
 
 test.skip('L2 | Font > Type | C769875 | PREAPPS-250', async t => {
@@ -535,19 +382,6 @@ test('L1 | Verify the insert link with respect to the cursor position | C871114 
 		.expect((await elements.richtextareaTextContentSelector.innerText).split(emailBodyText).length - 1).eql(1);
 });
 
-test.skip('L2 | Hyperlink > Search for Web Link | C828576 | PREAPPS-305 ', async t => {
-	let searchText = 'shopping';
-	  await compose.selectComposeToolbarPopmenu('Link', 'Search For Web Link');
-	  await t.wait(500);
-	  await compose.clickSuggestedSearchButton(searchText);
-	  await t
-		  .expect(elements.plusMenuBlockSpinner.exists).notOk({ timeout: 30000 })
-		  .expect(elements.plusSignMenuSearchesItemButton(0).innerText).contains('Shopping', 'verify search result contains shopping')
-		  .expect(elements.buttonWithText(searchText).exists).notOk();
-	  await compose.clearComposeSearchText();
-	  await t.expect(elements.buttonWithText(searchText).exists).ok({ timeout: 5000 });
-});
-
 test.skip('L1 | Emoticon button | C828577 | PREAPPS-250', async t => {
 	const expectedEmojiData = await compose.insertEmoji(0);
 	const actualEmojiData = await elements.richtextareaTextContentSelector.find('img').getAttribute('src');
@@ -566,9 +400,9 @@ fixture `Mail: Forward functions single user`
 	.beforeEach( async t => {
 		t.ctx.user = await soap.createAccount(t.fixtureCtx.adminAuthToken);
 		t.ctx.userAuth = await soap.getUserAuthToken(t.ctx.user.email, t.ctx.user.password);
-		const lmtp = new LmtpClient();
+		const inject = new Inject();
 		const filePath = path.join(__dirname, './data/mime/emails/empty.txt');
-		await lmtp.send(t.ctx.user.email, filePath);
+		inject.send(t.ctx.userAuth, filePath);
 		await t.maximizeWindow();
 		await actions.loginEmailPage(t.ctx.user.email, t.ctx.user.password);
 		await t.expect(sidebar.checkSidebarItemExists('Inbox')).ok({ timeout: 15000 });
