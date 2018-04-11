aa95eaedd63105891891830a206c432ee8fce216
Peter Lenahan <peter.lenahan@synacor.com>
2018-04-11 11:31:49 -0400

    PREAPPS-383: Set emoji height/width in CSS instead of as attributes. Solves inconsistency in emoji display between Chrome/Firefox.

--- src/components/composer/vhtml-templates/emoji-img.js
+++ src/components/composer/vhtml-templates/emoji-img.js
@@ -3,6 +3,6 @@ import { getSafeHtmlId } from '../../../lib/html-viewer';
 
 export default function EmojiImg({ contentId, url, name }) {
 	return clean(`
-		<img data-safe-html="${getSafeHtmlId()}" width="18" height="18" data-cid="${contentId}" src="${url}" alt="${name}" emoji>
+		<img data-safe-html="${getSafeHtmlId()}" data-cid="${contentId}" src="${url}" alt="${name}" emoji>
 	`);
 }
