d8334e7bb554cede29ffabe69d8d63b7afb27b24
Prashant Sharma <prashant.sharma@pnq-psharma.local>
2018-04-19 19:24:38 +0530

    PREAPPS-296 Small change in render method

--- src/components/calendar-add-event/calendar-add-event-toolbar.js
+++ src/components/calendar-add-event/calendar-add-event-toolbar.js
@@ -4,7 +4,7 @@ import Toolbar from '../toolbar';
 import CalendarAddEventAction from './calendar-add-event-action';
 import s from './style.less';
 
-export default function CalendarAddEventToolbar({ onSave, onCancel, isMobileActive, ...props }) {
+export default function CalendarAddEventToolbar({ isMobileActive, ...props }) {
 	const c = <CalendarAddEventAction class={isMobileActive ? s.toolbarAction : s.footer}  {...props} />;
 	return isMobileActive ? <Toolbar>{c}</Toolbar> : c;
 }
