6810ea914c1a65dd9ff150612a763f9ee6138cdb
Sijo Cheeran <sijo.cheeran@synacor.com>
2018-04-30 21:38:01 -0400

    Indentation fix

--- tests/e2e/testcafe/settings/viewmail.js
+++ tests/e2e/testcafe/settings/viewmail.js
@@ -44,16 +44,16 @@ test('L1 | Set preview pane to none and verify actions | C711417', async t => {
 	await settings.viewingEmail.selectPreviewPaneOption('None');
 	await settings.clickModalDialogFooterButton('Save');
 	await t.expect(await mail.getViewMailPanel()).eql('None', 'Verify the Panel View');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject2','body2');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject3','body3');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject2','body2');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject3','body3');
     
-    await actions.clickNavBarMenuItem('Mail');
-    await sidebar.clickSidebarContent('Inbox');
-    await mail.openEmail(0);
-    await t.expect(await mail.getMailBodyContent('None')).contains('body3','Verify the body content');
-    await mail.clickMessageNavigator('DownArrow');
-    await t.expect(await mail.getMailBodyContent('None')).contains('body2','Verify the body content');
+	await actions.clickNavBarMenuItem('Mail');
+	await sidebar.clickSidebarContent('Inbox');
+	await mail.openEmail(0);
+	await t.expect(await mail.getMailBodyContent('None')).contains('body3','Verify the body content');
+	await mail.clickMessageNavigator('DownArrow');
+	await t.expect(await mail.getMailBodyContent('None')).contains('body2','Verify the body content');
     
 
 });
@@ -64,15 +64,15 @@ test('L2 | Set preview pane to none and tap up arrow on viewing first email | C7
 	await settings.viewingEmail.selectPreviewPaneOption('None');
 	await settings.clickModalDialogFooterButton('Save');
 	await t.expect(await mail.getViewMailPanel()).eql('None', 'Verify the Panel View');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject2','body2');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject2','body2');
     
-    await actions.clickNavBarMenuItem('Mail');
-    await sidebar.clickSidebarContent('Inbox');
-    await mail.openEmail(0);
-    await t.expect(await mail.getMailBodyContent('None')).contains('body2','Verify the body content');
-    await mail.clickMessageNavigator('UpArrow');
-    await t.expect(await mail.getToastMessage()).contains('You have reached the first message in Inbox.','Verify the Toast message');
+	await actions.clickNavBarMenuItem('Mail');
+	await sidebar.clickSidebarContent('Inbox');
+	await mail.openEmail(0);
+	await t.expect(await mail.getMailBodyContent('None')).contains('body2','Verify the body content');
+	await mail.clickMessageNavigator('UpArrow');
+	await t.expect(await mail.getToastMessage()).contains('You have reached the first message in Inbox.','Verify the Toast message');
 
 });
 
@@ -82,66 +82,66 @@ test('L2 | Set preview pane to none and tap down arrow on viewing last email | C
 	await settings.viewingEmail.selectPreviewPaneOption('None');
 	await settings.clickModalDialogFooterButton('Save');
 	await t.expect(await mail.getViewMailPanel()).eql('None', 'Verify the Panel View');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject2','body2');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject2','body2');
     
-    await actions.clickNavBarMenuItem('Mail');
-    await sidebar.clickSidebarContent('Inbox');
-    await mail.openEmail(1);
-    await t.expect(await mail.getMailBodyContent('None')).contains('body1','Verify the body content');
-    await mail.clickMessageNavigator('DownArrow');
-    await t.expect(await mail.getToastMessage()).contains('You have reached the last message in Inbox.','Verify the Toast message');
+	await actions.clickNavBarMenuItem('Mail');
+	await sidebar.clickSidebarContent('Inbox');
+	await mail.openEmail(1);
+	await t.expect(await mail.getMailBodyContent('None')).contains('body1','Verify the body content');
+	await mail.clickMessageNavigator('DownArrow');
+	await t.expect(await mail.getToastMessage()).contains('You have reached the last message in Inbox.','Verify the Toast message');
 
 });
 
-test('L1 | Enabled view snippets from viewing email | C711420', async t=> {
-    await settings.clickSettings();
+test('L1 | Enabled view snippets from viewing email | C711420', async t => {
+	await settings.clickSettings();
 	await settings.clickSettingSidebarItem('Viewing Email');
-    await settings.viewingEmail.selectPreviewPaneOption('Preview pane on the right');
-    await settings.viewingEmail.enableShowSnippets(true);
-    await settings.clickModalDialogFooterButton('Save');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await actions.clickNavBarMenuItem('Mail');
-    await sidebar.clickSidebarContent('Inbox');
-    await t.expect(await mail.verifyMessageSnippets('Subject1','body1')).ok('Verify messsage snippets value');
-   
-});
-
-test('L2 | Disabled view snippets from viewing email | C711421', async t=> {
-    await settings.clickSettings();
-	await settings.clickSettingSidebarItem('Viewing Email');
-    await settings.viewingEmail.selectPreviewPaneOption('Preview pane on the right');
-    await settings.viewingEmail.enableShowSnippets(false);
-    await settings.clickModalDialogFooterButton('Save');
-    await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await actions.clickNavBarMenuItem('Mail');
-    await sidebar.clickSidebarContent('Inbox');
-    await t.expect(await mail.verifyMessageSnippets('Subject1','body1')).notOk('Verify messsage snippets value');
-   
-});
-
-
-test('L1 | Enable desktop notification | C714455', async t=> {
-    await settings.clickSettings();
-	await settings.clickSettingSidebarItem('Viewing Email');
-    await settings.viewingEmail.enableDesktopNotificaiton(true);
-    await settings.clickModalDialogFooterButton('Save');
-});
-
-test('L1 | Disable desktop notification | C714456', async t=> {
-    await settings.clickSettings();
-	await settings.clickSettingSidebarItem('Viewing Email');
-    await settings.viewingEmail.enableDesktopNotificaiton(false);
-    await settings.clickModalDialogFooterButton('Save');
-});
-
-test('L1 | Mark as read set to immediately | C714457', async t=> {
-    await settings.clickSettings();
-	await settings.clickSettingSidebarItem('Viewing Email');
-    await settings.viewingEmail.selectMarkasReadOption('Immediately');
+	await settings.viewingEmail.selectPreviewPaneOption('Preview pane on the right');
+	await settings.viewingEmail.enableShowSnippets(true);
 	await settings.clickModalDialogFooterButton('Save');
 	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await actions.clickNavBarMenuItem('Mail');
+	await actions.clickNavBarMenuItem('Mail');
+	await sidebar.clickSidebarContent('Inbox');
+	await t.expect(await mail.verifyMessageSnippets('Subject1','body1')).ok('Verify messsage snippets value');
+   
+});
+
+test('L2 | Disabled view snippets from viewing email | C711421', async t => {
+	await settings.clickSettings();
+	await settings.clickSettingSidebarItem('Viewing Email');
+	await settings.viewingEmail.selectPreviewPaneOption('Preview pane on the right');
+	await settings.viewingEmail.enableShowSnippets(false);
+	await settings.clickModalDialogFooterButton('Save');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
+	await actions.clickNavBarMenuItem('Mail');
+	await sidebar.clickSidebarContent('Inbox');
+	await t.expect(await mail.verifyMessageSnippets('Subject1','body1')).notOk('Verify messsage snippets value');
+   
+});
+
+
+test('L1 | Enable desktop notification | C714455', async t => {
+	await settings.clickSettings();
+	await settings.clickSettingSidebarItem('Viewing Email');
+	await settings.viewingEmail.enableDesktopNotificaiton(true);
+	await settings.clickModalDialogFooterButton('Save');
+});
+
+test('L1 | Disable desktop notification | C714456', async t => {
+	await settings.clickSettings();
+	await settings.clickSettingSidebarItem('Viewing Email');
+	await settings.viewingEmail.enableDesktopNotificaiton(false);
+	await settings.clickModalDialogFooterButton('Save');
+});
+
+test('L1 | Mark as read set to immediately | C714457', async t => {
+	await settings.clickSettings();
+	await settings.clickSettingSidebarItem('Viewing Email');
+	await settings.viewingEmail.selectMarkasReadOption('Immediately');
+	await settings.clickModalDialogFooterButton('Save');
+	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
+	await actions.clickNavBarMenuItem('Mail');
 	await sidebar.clickSidebarContent('Inbox');
 	await mail.clickonMessage('Subject1');
 	await t.wait(1000);
@@ -149,39 +149,39 @@ test('L1 | Mark as read set to immediately | C714457', async t=> {
 });
 
 
-test('L2 | Mark as read set to 2 secs | C714458', async t=> {
-    await settings.clickSettings();
+test('L2 | Mark as read set to 2 secs | C714458', async t => {
+	await settings.clickSettings();
 	await settings.clickSettingSidebarItem('Viewing Email');
-    await settings.viewingEmail.selectMarkasReadOption('After 2 seconds');
+	await settings.viewingEmail.selectMarkasReadOption('After 2 seconds');
 	await settings.clickModalDialogFooterButton('Save');
 	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await actions.clickNavBarMenuItem('Mail');
+	await actions.clickNavBarMenuItem('Mail');
 	await sidebar.clickSidebarContent('Inbox');
 	await mail.clickonMessage('Subject1');
 	await t.wait(3000);
 	await t.expect(await mail.isMessageRead('Subject1')).ok('Verify that message has been read');
 });
 
-test('L2 | Mark as read set to 5 secs | C714459', async t=> {
-    await settings.clickSettings();
+test('L2 | Mark as read set to 5 secs | C714459', async t => {
+	await settings.clickSettings();
 	await settings.clickSettingSidebarItem('Viewing Email');
-    await settings.viewingEmail.selectMarkasReadOption('After 5 seconds');
+	await settings.viewingEmail.selectMarkasReadOption('After 5 seconds');
 	await settings.clickModalDialogFooterButton('Save');
 	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await actions.clickNavBarMenuItem('Mail');
+	await actions.clickNavBarMenuItem('Mail');
 	await sidebar.clickSidebarContent('Inbox');
 	await mail.clickonMessage('Subject1');
 	await t.wait(6000);
 	await t.expect(await mail.isMessageRead('Subject1')).ok('Verify that message has been read');
 });
 
-test('L2 | Mark as read set to Never | C714460', async t=> {
-    await settings.clickSettings();
+test('L2 | Mark as read set to Never | C714460', async t => {
+	await settings.clickSettings();
 	await settings.clickSettingSidebarItem('Viewing Email');
-    await settings.viewingEmail.selectMarkasReadOption('Never');
+	await settings.viewingEmail.selectMarkasReadOption('Never');
 	await settings.clickModalDialogFooterButton('Save');
 	await soap.sendMessage(t.ctx.userAuth,t.ctx.user.email,'Subject1','body1');
-    await actions.clickNavBarMenuItem('Mail');
+	await actions.clickNavBarMenuItem('Mail');
 	await sidebar.clickSidebarContent('Inbox');
 	await mail.clickonMessage('Subject1');
 	await t.wait(10000);
